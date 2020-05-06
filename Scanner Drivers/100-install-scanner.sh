9a7fa90d91f2101916289a7d8785c888d26efe77
jdaeira <jdaeira@gmail.com>
2020-05-05 17:21:02 -0700

    added Scanner Drivers and Setup Folder

---
+++ Scanner Drivers/100-install-scanner.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+########################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+########################################################################
+
+sudo dpkg -i brscan4-0.4.8-1.amd64.deb
+sudo dpkg -i brscan-skey-0.2.4-1.amd64.deb
+sudo dpkg -i brother-udev-rule-type1-1.0.2-0.all.deb
+
+
+echo "############ Scanner Setup Complete #############"
+
